3f809493e46eb390362eda0ffda67020488bc614
fhira21 <fhiramaulani2105@gmail.com>
2023-07-25 00:28:54 +0700

    main

--- index.js
+++ index.js
@@ -4,16 +4,32 @@ import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
 
 const app = express();
-mongoose.connect('mongodb+srv://fhiramaulani2105:<password>@cluster0.w4htdpk.mongodb.net/?retryWrites=true&w=majority',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+
+mongoose.connect('mongodb+srv://fhiramaulani2105:Ftm210503_@cluster0.w4htdpk.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
+
 const db = mongoose.connection;
 db.on('error', (error) => console.log(error));
 db.once('open', () => console.log('Database Connected...'));
 
-app.use(cors());
+const allowedOrigins = ['http://example1.com', 'http://example2.com'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origin tidak diizinkan oleh CORS'));
+    }
+  },
+  allowedHeaders: ['X-Requested-With', 'Content-Type', 'Authorization'],
+  allowedMethods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(UserRoute);
 
-app.listen(5000, ()=> console.log('Server up and running...'));
+app.listen(5000, () => console.log('Server up and running...'));
